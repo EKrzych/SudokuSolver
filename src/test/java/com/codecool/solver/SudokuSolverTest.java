2988b9ba5cff43fe4f98f6e006c97b9d679f4feb
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-11-14 15:16:40 +0100

    Set SpringBoot for test.

---
+++ src/test/java/com/codecool/solver/SudokuSolverTest.java
@@ -0,0 +1,31 @@
+package com.codecool.solver;
+
+import com.codecool.model.Sudoku;
+import com.codecool.reader.SudokuReader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SudokuSolverTest {
+
+
+    @Autowired
+    private SudokuReader sudokuReader;
+
+    @Test
+    public void shoudRecogniseSolvedSudoku() {
+        Sudoku sudoku = sudokuReader.createSudoku("src/test/resources/sudoku.csv");
+        SudokuSolver sudokuSolver = new SudokuSolver(sudoku);
+
+        assertFalse(sudokuSolver.isSudokuSolved());
+
+    }
+
+
+}
